5c511a042e1299c20ccec2e86e2e03ee5bdf1e94
bkzhou <zhoubk1451@163.com>
2015-09-25 16:50:38 +0800

    拍照界面隐藏标题

--- app/src/main/java/com/example/bkzhou/fragment/CrimeCameraActivity.java
+++ app/src/main/java/com/example/bkzhou/fragment/CrimeCameraActivity.java
@@ -1,6 +1,9 @@
 package com.example.bkzhou.fragment;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by bkzhou on 15-9-23.
@@ -11,5 +14,12 @@ public class CrimeCameraActivity extends  SingleFragmentActivity {
         return new CrimeCameraFragment();
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 
+        super.onCreate(savedInstanceState);
+
+    }
 }
